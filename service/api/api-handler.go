631e56cbc39df1dcb4c0e97c4fa5b99bf2390597
u-siri-ous <sannino.2001580@studenti.uniroma1.it>
2024-11-12 20:58:45 +0100

    api-handler and api.yaml

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -9,7 +9,7 @@ func (rt *_router) Handler() http.Handler {
 	// Login
 	rt.router.POST("/login", rt.wrap(rt.Login, false))
 	// Search Users
-	rt.router.GET("/users", rt.wrap(rt.SearchUsers, true))
+	rt.router.GET("/users/", rt.wrap(rt.SearchUsers, true))
 	// Get User
 	rt.router.GET("/users/:userId", rt.wrap(rt.GetUser, true))
 	// Settings
@@ -17,19 +17,19 @@ func (rt *_router) Handler() http.Handler {
 	// Block
 	rt.router.PUT("/block/:userId", rt.wrap(rt.BlockUser, true))
 	rt.router.DELETE("/block/:userId", rt.wrap(rt.UnblockUser, true))
-	rt.router.GET("/blocked", rt.wrap(rt.BlockedUsers, true))
+	rt.router.GET("/blocked/", rt.wrap(rt.BlockedUsers, true))
 	// Follow
-	rt.router.PUT("/follow/:userId", rt.wrap(rt.FollowUser, true))
-	rt.router.DELETE("/follow/:userId", rt.wrap(rt.UnfollowUser, true))
 	rt.router.GET("/followers/:userId", rt.wrap(rt.GetFollowers, true))
 	rt.router.GET("/following/:userId", rt.wrap(rt.GetFollowing, true))
+	rt.router.PUT("/following/:userId", rt.wrap(rt.FollowUser, true))
+	rt.router.DELETE("/following/:userId", rt.wrap(rt.UnfollowUser, true))
 	// Post
-	rt.router.POST("/posts", rt.wrap(rt.CreatePost, true))
+	rt.router.POST("/posts/", rt.wrap(rt.CreatePost, true))
 	rt.router.DELETE("/posts/:postId", rt.wrap(rt.DeletePost, true))
-	rt.router.GET("/posts/:postId/likes", rt.wrap(rt.Likes, true))
-	rt.router.POST("/posts/:postId/likes", rt.wrap(rt.LikePost, true))
-	rt.router.DELETE("/posts/:postId/likes", rt.wrap(rt.UnlikePost, true))
-	rt.router.POST("/posts/:postId/comments", rt.wrap(rt.CommentPost, true))
+	rt.router.GET("/posts/:postId/likes/self", rt.wrap(rt.Likes, true))
+	rt.router.POST("/posts/:postId/likes/self", rt.wrap(rt.LikePost, true))
+	rt.router.DELETE("/posts/:postId/likes/self", rt.wrap(rt.UnlikePost, true))
+	rt.router.POST("/posts/:postId/comments/", rt.wrap(rt.CommentPost, true))
 	rt.router.DELETE("/posts/:postId/comments/:commentId", rt.wrap(rt.DeleteCommentPost, true))
 
 	return rt.router
