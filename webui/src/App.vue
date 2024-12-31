64cc5575fe976c1cf9fc9d28d3d8ec4b138d563f
u-siri-ous <sannino.2001580@studenti.uniroma1.it>
2024-12-31 18:45:46 +0100

    create post correctly

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -22,7 +22,7 @@ export default {
 				this.$router.push('/users/' + sessionStorage.getItem('userId'));
 			}
 		}
-    },
+	},
 	methods: {
 		logout() {
 			console.log("LOGOUT");
@@ -38,7 +38,9 @@ export default {
 
 	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
 		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">Example App</a>
-		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse"
+			data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+			aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 	</header>
@@ -47,44 +49,64 @@ export default {
 		<div class="row">
 			<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
 				<div class="position-sticky pt-3 sidebar-sticky">
-					<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+					<h6
+						class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
 						<span>General</span>
 					</h6>
 					<ul class="nav flex-column">
-						<li v-if="username===null" class="nav-item">
+						<li v-if="username === null" class="nav-item">
 							<RouterLink to="/" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#home"/></svg>
+								<svg class="feather">
+									<use href="/feather-sprite-v4.29.0.svg#home" />
+								</svg>
 								Login
 							</RouterLink>
 						</li>
-						<li v-if="username!==null" class="nav-item">
+						<li v-if="username !== null" class="nav-item">
 							<RouterLink :to="'/users/'" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#search"/></svg>
+								<svg class="feather">
+									<use href="/feather-sprite-v4.29.0.svg#search" />
+								</svg>
 								Explore Users
 							</RouterLink>
 						</li>
 					</ul>
 
-					<div v-if="username!==null">
-						<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+					<div v-if="username !== null">
+						<h6
+							class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
 							<span>Actions</span>
 						</h6>
 						<ul class="nav flex-column">
 							<li class="nav-item">
 								<RouterLink :to="loggedpath" class="nav-link">
-									<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#user"/></svg>
+									<svg class="feather">
+										<use href="/feather-sprite-v4.29.0.svg#user" />
+									</svg>
 									Profile
 								</RouterLink>
 							</li>
 							<li class="nav-item">
+								<RouterLink to="/posts" class="nav-link">
+									<svg class="feather">
+										<use href="/feather-sprite-v4.29.0.svg#edit" />
+									</svg>
+									Create Post
+								</RouterLink>
+							</li>
+							<li class="nav-item">
 								<RouterLink to="/settings" class="nav-link">
-									<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#settings"/></svg>
+									<svg class="feather">
+										<use href="/feather-sprite-v4.29.0.svg#settings" />
+									</svg>
 									Settings
 								</RouterLink>
 							</li>
 							<li class="nav-item">
 								<RouterLink to="/logout" @click="logout" class="nav-link">
-									<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#log-out"/></svg>
+									<svg class="feather">
+										<use href="/feather-sprite-v4.29.0.svg#log-out" />
+									</svg>
 									Logout
 								</RouterLink>
 							</li>
@@ -100,5 +122,4 @@ export default {
 	</div>
 </template>
 
-<style>
-</style>
+<style></style>
--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -4,6 +4,7 @@ import LogoutView from '../views/LogoutView.vue'
 import UserView from '../views/UserView.vue'
 import ExploreUsers from '../views/ExploreUsers.vue'
 import ChangeUsernameView from '../views/ChangeUsernameView.vue'
+import CreatePostView from '../views/CreatePostView.vue'
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,7 @@ const router = createRouter({
 		{path: '/logout', component: LogoutView},
 		{path: '/users/', component: ExploreUsers},
 		{path: '/settings', component: ChangeUsernameView},
+		{path: '/posts', component: CreatePostView},
 		{path: '/users/:userId', component: UserView},
 	]
 })
